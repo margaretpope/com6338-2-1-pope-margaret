5e667ca7f95b633dd6d0f5132ae47c16cba7bee4
Margaret Pope <mmp298@gmail.com>
2023-09-03 12:24:24 -0500

    greet function

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Your code here
+function greet () {
+    var name = prompt('What is your name?')
+    if (!name) return
+    var hello = alert('Hello, ' + name)
+    var ageStr = prompt('How old are you?')
+    if (!ageStr) return
+    var age = parseInt(ageStr)
+    var birthday = confirm('Have you had a birthday yet this year?')
+    if (birthday) {
+        year = new Date().getFullYear() - age
+    } else {
+        year = new Date().getFullYear() - (age + 1)
+    } 
+    var answer = alert('You were born in ' + year)
+}
